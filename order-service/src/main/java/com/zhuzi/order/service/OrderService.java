85ac870b0a58fb3a251126dd40757f5868013ac8
zhuzi <sanyewudezhuzi@163.com>
2023-09-22 23:34:50 +0800

    使用RestTemplate实现远程调用

--- order-service/src/main/java/com/zhuzi/order/service/OrderService.java
+++ order-service/src/main/java/com/zhuzi/order/service/OrderService.java
@@ -2,8 +2,10 @@ package com.zhuzi.order.service;
 
 import com.zhuzi.order.mapper.OrderMapper;
 import com.zhuzi.order.pojo.Order;
+import com.zhuzi.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -11,9 +13,20 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        // 2. 获取url
+        String url = "http://localhost:8081/user/" + order.getUserId();
+
+        // 3. 获取user数据并进行封装
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+
         // 4.返回
         return order;
     }
