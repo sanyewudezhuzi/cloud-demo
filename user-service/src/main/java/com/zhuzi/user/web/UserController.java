b015f04062a616caa131803b0b67e7481531b4f4
zhuzi <sanyewudezhuzi@163.com>
2023-09-24 22:45:42 +0800

    gateway路由过滤器

--- user-service/src/main/java/com/zhuzi/user/web/UserController.java
+++ user-service/src/main/java/com/zhuzi/user/web/UserController.java
@@ -5,7 +5,6 @@ import com.zhuzi.user.pojo.User;
 import com.zhuzi.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RestController
 @RequestMapping("/user")
-@RefreshScope // 使用@Value读取配置数据实现热更新
+@RefreshScope
 public class UserController {
 
     @Autowired
@@ -32,15 +31,6 @@ public class UserController {
         return userService.queryById(id);
     }
 
-    // 为了验证test开发环境，需要注释这段代码
-    /* @Value("${pattern.dateformat}")
-    private String dateformat;
-
-    @GetMapping("/now")
-    public String now() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
-    } */
-
     @Autowired
     private PatternProperties patternProperties;
 
@@ -52,6 +42,11 @@ public class UserController {
     @GetMapping("/info")
     public String info() {
         return patternProperties.getEnvShareValue();
+    }
+
+    @GetMapping("/header")
+    public String header(@RequestHeader(name = "name", required = false) String name) {
+        return name;
     }
 
 }
